4a18c463da6d090dd3f91679060c3b89be827796
DeepRaman21 <raman29012001@gmail.com>
2024-04-22 13:37:37 +0530

    deep

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://mobile:mobile12@cluster0.7hhzq5p.mongodb.net/Blogapp")  //mongoose is a Scheme declared library of mdb
+//connection mongoose and mongodb
+const mongoose= require('mongoose')
+mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
-    console.log("Mongodb Connected")
-})
-const userSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String
+    console.log("mongoose Connected")
 });
-
-const User = mongoose.model("user",userSchema)
-
-module.exports = User;
+//schema
+const userSchema = new mongoose.Schema({
+    firstname:String,
+    lastname:String,
+    username:String,
+    email:String,
+    password:String
+})
+//connection mongoose with schema
+const user = mongoose.model("user",userSchema)
+module.exports = user;
